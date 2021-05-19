d698eb96cae53ffcf5f9ce0fbc0a0b7a0315a9cc
nazhtafza <nazhatafza1@gmail.com>
2021-05-18 18:32:59 -0700

    make detail invoices

---
+++ application/controllers/dashboard.php
@@ -0,0 +1,65 @@
+<?php
+
+class Dashboard extends CI_Controller
+{
+
+    public function index()
+    {
+        $data['barang'] = $this->model_barang->tampil_data()->result();
+
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('templates/footer');
+        $this->load->view('dashboard', $data);
+    }
+
+    public function tambah_keranjang($id)
+    {
+        $barang = $this->model_barang->find($id);
+        $data = array(
+            'id'      => $barang->id_barang,
+            'qty'     => 1,
+            'price'   => $barang->harga,
+            'name'    => $barang->nama_barang
+        );
+
+        $this->cart->insert($data);
+        redirect('dashboard');
+    }
+
+    public function detail_keranjang()
+    {
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('templates/footer');
+        $this->load->view('keranjang');
+    }
+
+    public function hapus_keranjang()
+    {
+        $this->cart->destroy();
+        redirect('dashboard/index');
+    }
+
+    public function pembayaran()
+    {
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('templates/footer');
+        $this->load->view('pembayaran');
+    }
+
+    public function proses_pesanan()
+    {
+        $is_processed = $this->model_invoice->index();
+        if ($is_processed) {
+            $this->cart->destroy();
+            $this->load->view('templates/header');
+            $this->load->view('templates/sidebar');
+            $this->load->view('proses_pesanan');
+            $this->load->view('templates/footer');
+        } else {
+            echo "Maaf, Pesanan Anda gagal diproses!";
+        }
+    }
+}
---
+++ application/views/pembayaran.php
@@ -0,0 +1,65 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+            <div class="btn btn-sm btn-success">
+                <?php
+                $grand_total = 0;
+                if ($keranjang = $this->cart->contents()) {
+                    foreach ($keranjang as $item) {
+                        $grand_total = $grand_total + $item['subtotal'];
+                    }
+                    echo "<h4>Total belanja anda : Rp " . number_format($grand_total, 0, ',', '.');
+
+                ?>
+
+            </div><br><br>
+            <h3>Input Alamat Pengiriman dan Pembayaran</h3>
+
+            <form action="<?php echo base_url('dashboard/proses_pesanan'); ?> " method="post">
+                <div class="form-group">
+                    <label>Nama Lengkap</label>
+                    <input type="text" name="nama" placeholder="Nama Lengkap Anda" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label>Alamat Lengkap</label>
+                    <input type="text" name="alamat" placeholder="Alamat Lengkap Anda" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label>Nomor Telepon</label>
+                    <input type="text" name="no_telp" placeholder="Nomor Telepon Anda" class="form-control">
+                </div>
+
+                <div class="form-group form-control">
+                    <label>Jasa Pengiriman</label>
+                    <select>
+                        <option>JNE</option>
+                        <option>TIKI</option>
+                        <option>POS INDONESIA</option>
+                    </select>
+                </div>
+
+                <div class="form-group form-control">
+                    <label>Pembayaran</label>
+                    <select>
+                        <option>BCA - XXXX</option>
+                        <option>BNI - XXXX</option>
+                        <option>BRI - XXXX</option>
+                    </select>
+                </div>
+
+                <button type="submit" class="btn btn-sm btn-primary">Pesan</button>
+            </form>
+
+        <?php
+                } else {
+                    echo "<h4>Keranjang belanja anda masih kosong";
+                }
+        ?>
+        </div>
+
+        <div class="col-md-2"></div>
+    </div>
+</div>
